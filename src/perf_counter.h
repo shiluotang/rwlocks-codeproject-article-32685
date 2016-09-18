3f5c5246f070e6c927bc5320f1f982be6d9f7cdd
Quangang Sheng <quangang.sheng@wz-inc.com>
2016-09-18 20:27:07 +0800

    Use _M_xx for field names.

--- src/perf_counter.h
+++ src/perf_counter.h
@@ -1,52 +1,50 @@
 #pragma once
+
 #include <stdexcept>
-class Performance_counter_meter
-{
+
+class PerformanceCounterMeter {
 public:
-	Performance_counter_meter()
-	{
-		m_counter = 0L;
-		m_frequency.QuadPart = m_performance_counter_start.QuadPart = m_duration.QuadPart = 0L;
-		if ( FALSE == ::QueryPerformanceFrequency(&m_frequency) )
-		{
+	PerformanceCounterMeter() {
+		_M_counter = 0L;
+		_M_frequency.QuadPart = _M_perf_counter_start.QuadPart = _M_duration.QuadPart = 0L;
+		if (FALSE == ::QueryPerformanceFrequency(&_M_frequency))
 			throw std::runtime_error("Failed to initialize performance counter."); 
-		}
 	}
-	bool start(void)
-	{
-		return (::QueryPerformanceCounter(&m_performance_counter_start) != FALSE);
+
+	bool start() {
+		return (::QueryPerformanceCounter(&_M_perf_counter_start) != FALSE);
 	}
-	bool end(void)
-	{
+
+	bool end() {
 		LARGE_INTEGER performance_counter;
 		bool rc = (::QueryPerformanceCounter(&performance_counter) != FALSE);
-		m_duration.QuadPart += performance_counter.QuadPart - m_performance_counter_start.QuadPart;
+		_M_duration.QuadPart += performance_counter.QuadPart - _M_perf_counter_start.QuadPart;
 		return rc;
 	}
-	double get_duration(void)
-	{
-		return (double)m_duration.QuadPart / m_frequency.QuadPart;
+
+	double get_duration() {
+		return (double) _M_duration.QuadPart / _M_frequency.QuadPart;
 	}
-	bool get_current_duration(double& duration)
-	{
+
+	bool get_current_duration(double& duration) {
 		LARGE_INTEGER performance_counter, counter_duration;
 		bool rc = (::QueryPerformanceCounter(&performance_counter) != FALSE);
-		counter_duration.QuadPart = performance_counter.QuadPart - m_performance_counter_start.QuadPart;
-		duration = (double)counter_duration.QuadPart / m_frequency.QuadPart;
+		counter_duration.QuadPart = performance_counter.QuadPart - _M_perf_counter_start.QuadPart;
+		duration = (double) counter_duration.QuadPart / _M_frequency.QuadPart;
 		return rc;
 	}
-	LONGLONG get_counter(void)
-	{
-		return m_counter;
+
+	LONGLONG get_counter() {
+		return _M_counter;
 	}
-	void set_iteration_done(LONGLONG iteration_done)
-	{
-		m_counter = iteration_done;
+
+	void set_iteration_done(LONGLONG iteration_done) {
+		_M_counter = iteration_done;
 	}
 
 private:
-	LARGE_INTEGER m_frequency;
-	LARGE_INTEGER m_performance_counter_start;
-	LARGE_INTEGER m_duration;
-	LONGLONG m_counter;
+	LARGE_INTEGER _M_frequency;
+	LARGE_INTEGER _M_perf_counter_start;
+	LARGE_INTEGER _M_duration;
+	LONGLONG _M_counter;
 };
